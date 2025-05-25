c96258b1981d3dcb843fae5203db8e046a3513f7
Naveen <124484861+i0xnaveen@users.noreply.github.com>
2025-05-25 14:37:19 +0000

    Added sql connections

---
+++ back/models/organizations.model.js
@@ -0,0 +1,16 @@
+module.exports  = function OrganizationModel(sequelize, Datatypes) {
+
+    const Organization = sequelize.define('Organization',{
+        id: {type: Datatypes.STRING, primaryKey: true},
+        name: {type: Datatypes.STRING, unique: true},
+        features: {type: Datatypes.STRING},
+        isActive: {type: Datatypes.BOOLEAN},
+
+    },{
+        tableName: 'organizations'
+    }
+
+)
+    return Organization
+
+}
