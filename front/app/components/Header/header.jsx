cbd458af796f42f8f27f6bf4be14b5bc8932ce4a
Naveen <124484861+i0xnaveen@users.noreply.github.com>
2025-11-09 14:47:28 +0000

    Merge commit fixed

--- front/app/components/Header/header.jsx
+++ front/app/components/Header/header.jsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import React from "react";
-
-const Header = ({icons})=>{
-    return(
-<Header style={{ backgroundColor: '#012E5B' }}>
-            <div className="flex space-x-4">
-                {icons.map((IconComponent,index)=>(
-                    <IconComponent key={index} className="w-6 h-6 cursor-pointer hover:text-yellow-300" />
-                ))}
-            </div>
-        </Header>
-        
-    )
-
-}
-=======
 import React from "react"
 
 const Header = ()=>(
@@ -24,5 +7,4 @@ const Header = ()=>(
   </div>
         
 )
->>>>>>> 4bf54ea4 (Naveen (#3))
 export default Header
